fa50b9f373885e765d083ccd9473385af71aeee5
igorek1955 <igorek1955@gmail.com>
2022-03-10 18:16:44 +0300

    commit 1 in main

--- app/src/main/java/com/example/recipesapp/ui/fragments/main/FoodJokeFragment.kt
+++ app/src/main/java/com/example/recipesapp/ui/fragments/main/FoodJokeFragment.kt
@@ -42,6 +42,7 @@ class FoodJokeFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.food_joke_menu, menu)
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
